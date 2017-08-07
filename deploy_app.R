3afc88cd506277b9cedc8248ff2b206805b48e76
Hansen Johnson <hansenjohnson@DF1D5.VPN.Dal.Ca>
2017-08-07 13:36:49 -0300

    update app deployment directory

--- deploy_app.R
+++ deploy_app.R
@@ -1,4 +1,4 @@
 # deploy 2017 right whale sightings app
 
 library(rsconnect)
-rsconnect::deployApp('~/Projects/2017_right_whale_map/', appName = '2017_right_whale_map')
+rsconnect::deployApp('~/Projects/interactive_map/', appName = '2017_right_whale_map')
