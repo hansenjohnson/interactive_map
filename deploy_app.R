1d36ee052db146d5d5db4d2d7498b77e9af07591
Hansen Johnson <hansenjohnson@Hansens-MacBook-Pro-2.local>
2017-08-07 16:48:07 -0300

    specify necessary files for app deployment

--- deploy_app.R
+++ deploy_app.R
@@ -1,4 +1,7 @@
 # deploy 2017 right whale sightings app
 
 library(rsconnect)
-rsconnect::deployApp('~/Projects/interactive_map/', appName = '2017_right_whale_map')
+
+rsconnect::deployApp(appDir = '~/Projects/interactive_map/', 
+                     appFiles = c('app.R', 'noaa_tracks.rda'), 
+                     appName = '2017_right_whale_map')
