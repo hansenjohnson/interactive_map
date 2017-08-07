fc4e7fe08c680fc0e70c8b9a3be5906b56db29dd
Hansen Johnson <hansenjohnson@Hansens-MacBook-Pro-2.local>
2017-08-07 17:54:31 -0300

    fixed last timestamp glitch

--- app.R
+++ app.R
@@ -54,14 +54,14 @@ clean_latlon = function(d){
 
 proc_glider_kml = function(glider, download = T){
   message('Downloading and processing tracklines for ', glider)
-
+  
   s = proc.time()
   
   glider_file = paste0('./', glider, '.kml')
   glider_url = paste0('http://gliders.oceantrack.org/ge/', glider, '.kml')
   
   if(download){
-    download.file(url = glider_url, destfile = glider_file, quiet = T, mode = 'a')
+    download.file(url = glider_url, destfile = glider_file, quiet = T)
     message('File saved as: ', glider_file)
   }
   
@@ -69,16 +69,17 @@ proc_glider_kml = function(glider, download = T){
   surf = readOGR(glider_file, layer = paste0(glider, ' Surfacings'), verbose = F)
   glider = cbind.data.frame(surf@coords[,c(2,1)], as.character(surf$Name))
   colnames(glider) = c('lat', 'lon', 'time')
+  glider$time = as.character(glider$time)
   
-  # latest = glider[nrow(glider),] # latest surfacing
-  glider = glider[1:nrow(glider)-1,] # all other surfacings
+  # convert timestamp of latest surfacing
+  glider$time[nrow(glider)] = strsplit(x = as.character(glider$time[nrow(glider)]), split = '[()]')[[1]][2]
   
+  # fix date
   glider$time = as.POSIXct(glider$time, format = '%m-%d %H:%M')
   glider$date = as.Date(glider$time)
   
   message('Complete! Time elapsed = ', (proc.time()-s)[3])
   
-  # return(list(glider=glider,latest=latest))
   return(glider)
 }
 
@@ -176,7 +177,6 @@ server <- function(input, output, session) {
   # define groups -----------------------------------------------------------
   message('    ...defining plotting groups')
   
-  
   sightings_grp = paste0("Sightings [latest: ",
                          format(max(sightings$date), '%d-%b'),'; n = ', nrow(sightings),']')
   noaa_track_grp = paste0("NOAA plane tracks [latest: ",
@@ -222,7 +222,6 @@ server <- function(input, output, session) {
   
   # basemap -----------------------------------------------------------------
   message('    ...plotting basemap')
-  
   
   output$map <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
