8ed8bc55f43214e24388145f916cb72d647f8d8a
Hansen Johnson <hansenjohnson@Hansens-MacBook-Pro-2.local>
2017-08-23 07:22:10 -0300

    loop through glider files and cd to source r script

--- get_glider_tracks.sh
+++ get_glider_tracks.sh
@@ -1,13 +1,15 @@
+#!/bin/sh
 # download glider files
 
-# bond
-wget http://gliders.oceantrack.org/ge/bond.kml -O /home/hansen/shiny-server/whale_map/bond_tracks.kml
+DESTDIR=/home/hansen/shiny-server/whale_map # server
+# DESTDIR=/Users/hansenjohnson/Projects/interactive_map # local
+URL=http://gliders.oceantrack.org/ge
 
-# dal556
-wget http://gliders.oceantrack.org/ge/otn200.kml -O /home/hansen/shiny-server/whale_map/otn200_tracks.kml
-
-# otn200
-wget http://gliders.oceantrack.org/ge/dal556.kml -O /home/hansen/shiny-server/whale_map/dal556_tracks.kml
+for product in bond dal556 otn200
+do
+   wget -q ${URL}/${product}.kml -O ${DESTDIR}/${product}_tracks.kml
+   if [ 0 -ne $? ] ; then { echo "Failed downloading ${product}"; exit 1; }; fi
+done
 
 # run R script to process these data
-Rscript -e "source('/home/hansen/shiny-server/whale_map/proc_glider_tracks.R')"
+( cd ${DESTDIR}; Rscript -e "source('proc_glider_tracks.R')" )
