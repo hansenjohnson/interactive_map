e03b79121e1a5fd12a093b19e39e6b255d226b1b
Hansen Johnson <hansen.johnson@dal.ca>
2017-09-08 09:04:26 -0300

    move to dev - not important for day to day app use

--- deploy_app.R
+++
@@ -1,7 +0,0 @@
-# deploy 2017 right whale sightings app to shinyapps.io
-
-library(rsconnect)
-
-rsconnect::deployApp(appDir = '~/Projects/interactive_map/', 
-                     appFiles = c('app.R', 'noaa_tracks.rda', 'shelagh_tracks.rda', 'gs_auth.rds'), 
-                     appName = '2017_right_whale_map')
