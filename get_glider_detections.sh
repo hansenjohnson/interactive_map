96789fdd1184a7e45c2f48dc5e8a86da97332b95
Hansen Johnson <hansen.johnson@dal.ca>
2017-09-08 09:11:56 -0300

    script to download glider detection data

---
+++ get_glider_detections.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# download glider detection files
+
+# NOTE - this script makes use of associative arrays, 
+# which are only available in bash --version >=4.0
+
+# Choose destination of glider files
+DESTDIR=/srv/shiny-server/right_whale_map # server
+#DESTDIR=/Users/hansenjohnson/Projects/interactive_map # local
+
+# initiate array
+declare -A URL
+
+# assign paths to data for each glider
+URL=(
+	[dal556]=http://dcs.whoi.edu/dal0617_dal556/dal556_html/ptracks/manual_analysis.csv
+	[otn200]=http://dcs.whoi.edu/dal0817/dal0817_otn200_html/ptracks/manual_analysis.csv
+	[bond]=http://dcs.whoi.edu/dal0617_bond/bond_html/ptracks/manual_analysis.csv
+)
+
+# download data
+for i in "${!URL[@]}"; do   	
+	wget -q -N ${URL[$i]} ${DESTDIR}/${i}_manual_analysis.csv	
+done
