08a2c376e103d49c6dc264040a2173fed12f3374
Hansen Johnson <hansen@whalemap.ocean.lan>
2017-09-17 21:02:18 -0300

    formatting

--- app.R
+++ app.R
@@ -86,7 +86,7 @@ ui <- bootstrapPage(
   
   leafletOutput("map", width = "100%", height = "100%"),
   
-  absolutePanel(top = 10, right = 50,fixed = T, width='100px',
+  absolutePanel(top = 10, right = 50,fixed = T, width='200px',
                 h3(strong('2017 Right whale surveys'), align = 'center'),
                 h6(strong(paste0('CAUTION: raw data! Updating automatically...')), 
                    br(),
