ef96a380e3066ad8202d8ef91f4b61f73f9f9a8e
Hansen Johnson <hansen.johnson@dal.ca>
2017-09-21 16:47:16 -0300

    change dir

--- proc_dfo_tracks_gdrive.sh
+++ proc_dfo_tracks_gdrive.sh
@@ -2,8 +2,8 @@
 # download and process dfo plane trackline data using an R script
 
 # Select app directory
-#DESTDIR=/srv/shiny-server/right_whale_map # server
-DESTDIR=/Users/hansenjohnson/Projects/interactive_map # local
+DESTDIR=/srv/shiny-server/right_whale_map # server
+#DESTDIR=/Users/hansenjohnson/Projects/interactive_map # local
 
 # process data
 ( cd ${DESTDIR}; Rscript -e "source('proc_dfo_tracks_gdrive.R')" )
